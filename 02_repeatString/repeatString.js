5e8e9f43622c539c2be09d5f29e931c871a9f01f
Roan <17789917+RoanOpperman@users.noreply.github.com>
2024-09-13 15:52:58 -0600

    Repeat a string a number of times as per TOP instruction

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function (stringToRepeat, num) {
+  let resultString = "";
+  for (let i = 1; i <= num; i++) {
+    resultString += stringToRepeat;
+  }
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    return resultString;
+  }
 };
+repeatString("hey", 3);
+repeatString("hey", 10);
+repeatString("hi", 1);
+repeatString("bye", 0);
+repeatString("goodbye", -1);
+repeatString("", 12);
+repeatString("", 10);
 
 // Do not edit below this line
 module.exports = repeatString;
