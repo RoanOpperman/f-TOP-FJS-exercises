6ae4c43a71eb663a08f305859dd6fb1511e69413
Roan <17789917+RoanOpperman@users.noreply.github.com>
2024-09-13 16:24:43 -0600

    Fix double quote auto formatting

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,21 +1,21 @@
 const repeatString = function (stringToRepeat, num) {
-  let resultString = "";
+  let resultString = '';
   for (let i = 1; i <= num; i++) {
     resultString += stringToRepeat;
   }
   if (num < 0) {
-    return "ERROR";
+    return 'ERROR';
   } else {
     return resultString;
   }
 };
-repeatString("hey", 3);
-repeatString("hey", 10);
-repeatString("hi", 1);
-repeatString("bye", 0);
-repeatString("goodbye", -1);
-repeatString("", 12);
-repeatString("", 10);
+repeatString('hey', 3);
+repeatString('hey', 10);
+repeatString('hi', 1);
+repeatString('bye', 0);
+repeatString('goodbye', -1);
+repeatString('', 12);
+repeatString('', 10);
 
 // Do not edit below this line
 module.exports = repeatString;
