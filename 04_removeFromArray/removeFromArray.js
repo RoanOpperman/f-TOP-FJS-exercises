a696aef5ed69d0c1a680cd4e587468798888f913
Roan <17789917+RoanOpperman@users.noreply.github.com>
2024-09-17 17:18:59 -0600

    Remove items from arrays as per TOP assigment instructions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrayObject, ...inputs) {
+  const arrayArgument = arrayObject;
+  const referenceArgument = inputs;
+  const resultArr = [];
+  for (let i = 0; i < arrayArgument.length; i++) {
+    let enablePushResult = true;
+    for (let r = 0; r < referenceArgument.length; r++) {
+      if (arrayArgument[i] === referenceArgument[r]) {
+        enablePushResult = false;
+        break;
+      }
+    }
+    if (enablePushResult) {
+      resultArr.push(arrayArgument[i]);
+    }
+  }
+  return resultArr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (rangeStart, rangeEnd) {
+  let sum = 0;
+  let swapArgs = 0;
+  if (rangeStart > rangeEnd) {
+    swapArgs = rangeStart;
+    rangeStart = rangeEnd;
+    rangeEnd = swapArgs;
+  }
+  if (
+    !Number.isInteger(rangeStart) ||
+    !Number.isInteger(rangeEnd) ||
+    rangeStart < 0 ||
+    rangeEnd < 0
+  )
+    return "ERROR";
+  for (let i = rangeStart; i <= rangeEnd; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
