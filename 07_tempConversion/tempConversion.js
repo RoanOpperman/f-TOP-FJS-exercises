8411d47fa0b475ba7c0709da1d6419e9651c832a
Roan <17789917+RoanOpperman@users.noreply.github.com>
2024-09-18 18:37:01 -0600

    Convert tempratures (fahrenheit/celsius) resulting no more than 1 decimal place as per TOP excersise assignment

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInFahr) {
+  let tempCelsiusResult = (tempInFahr - 32) * (5 / 9);
+  return Number(tempCelsiusResult.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInCelsius) {
+  let tempFahrResult = (9 / 5) * tempInCelsius + 32;
+  return Number(tempFahrResult.toFixed(1));
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
